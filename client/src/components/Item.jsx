ab0ad01b3d0fca341a40c005ec7fb4bd300c7385
Claire Tsoi <clairetsoi1129@gmail.com>
2023-05-06 14:53:05 +0100

    fix - icon import error

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
-import {AddIcon, RemoveIcon} from "@mui/icons-material";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 import { shades } from "../theme";
 import { addToCart } from "../state";
 import { useNavigate } from "react-router-dom";
